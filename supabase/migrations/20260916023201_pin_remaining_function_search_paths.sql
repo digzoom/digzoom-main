5ae473dbfece6c6623ebfc9861618ef062de7de5
digzoom <mehran20092030@gmail.com>
2026-09-16 05:36:57 +0300

    security: pin remaining database function paths

---
+++ supabase/migrations/20260916023201_pin_remaining_function_search_paths.sql
@@ -0,0 +1,2 @@
+alter function public.set_updated_at() set search_path = pg_catalog, public;
+alter function public.search_products(text, integer, public.product_type) set search_path = pg_catalog, public;
